e672f8fd677954fde363ad4f534cdaf78f3b1e8a
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-01-27 16:52:24 -0600

    10. Refactored to add Home page

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+//REDUX
+import { useDispatch } from 'react-redux';
+import { loadGames } from './actions/gamesAction';
+
+const Home = () => {
+	//FETCH GAMES
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(loadGames());
+	});
+
+	return <h1>Home</h1>;
+};
